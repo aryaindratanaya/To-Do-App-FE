3625036cd3927cadc612e67bd81c07ea90ae6a26
Indra Tanaya <aryaindratanaya@gmail.com>
2022-01-03 14:50:09 +0800

    Update the meta tags in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
+import Head from 'next/head'
+
 import 'styles/antd.less'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>To Do App</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="A simple to do app. This is a working example of a to-do app using Next.JS."
+        />
+        <meta
+          name="og:title"
+          property="og:title"
+          content="A simple to do app. This is a working example of a to-do app using Next.JS."
+        ></meta>
+        <meta
+          name="twitter:card"
+          content="A simple to do app. This is a working example of a to-do app using Next.JS."
+        ></meta>
+        <link rel="canonical" href="#"></link>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
