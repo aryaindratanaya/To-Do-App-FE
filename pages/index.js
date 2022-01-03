533bc35b2a38a4d17f4fcba14b4b95f4117668d9
Indra Tanaya <aryaindratanaya@gmail.com>
2022-01-03 16:15:52 +0800

    Populate data to To Do Table

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import { Card, Table } from 'antd'
 
-import { columns } from 'utils/Home'
+import { columns } from 'utils/columns/Home'
 
 import s from 'styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <main>
       <Card title="To Do List" className={s.toDoCard}>
-        <Table columns={columns} dataSource={[]} />
+        <Table columns={columns} dataSource={data || []} />
       </Card>
     </main>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:8000`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data },
+  }
 }
